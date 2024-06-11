24433a3ccab86033dee913602028dc2ef04c1036
Aleksei Sizov <a13x5@proton.me>
2024-06-10 20:21:52 -0500

    Initial

---
+++ i2c-lcd.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/i2c-dev.h>
+#include "i2c-lcd.h"
+
+/*
+ * Low level data write function.
+ * Clock pin must be manually triggered each time data is transfered.
+ * Data is transfered 3 times. Ref. HD44780 manual page 22
+ */
+void
+clk_write(int fd, unsigned char byte)
+{
+    write(fd, &byte, 1);
+    byte |= PIN_E;
+    write(fd, &byte, 1);
+    usleep(1);
+    byte &= ~PIN_E;
+    write(fd, &byte, 1);
+    usleep(50);
+}
+
+/*
+ * Writing data in 4 bit mode. So it should be splitted and send 2 times.
+ * Ref. HD44780 manual page 22
+ * 0xF0 is used to mask bits that don't contain data. 
+ */
+void
+general_write(lcd_config *cfg, unsigned char byte, unsigned char mode)
+{
+    unsigned char high_order_bits = (byte & 0xF0) | mode | cfg->backlight;
+    unsigned char low_order_bits = ((byte << 4) & 0xF0) | mode | cfg->backlight;
+    clk_write(cfg->i2c_fd, high_order_bits);
+    clk_write(cfg->i2c_fd, low_order_bits);
+}
+
+/*
+ * HD44780 init sequence.
+ * Ref. HD44780 manual pages 23 and 46.
+ */
+void
+init_lcd(lcd_config *cfg)
+{
+    unsigned char byte;
+    usleep(4000);
+    byte = (PIN_DB5 | PIN_DB4 | cfg->backlight);
+    clk_write(cfg->i2c_fd, byte);
+    usleep(4500);
+    clk_write(cfg->i2c_fd, byte);
+    usleep(4500);
+    clk_write(cfg->i2c_fd, byte);
+    usleep(150);
+    byte = (PIN_DB5 | cfg->backlight);
+    clk_write(cfg->i2c_fd, byte);
+    
+    general_write(cfg, cfg->lcd_function, LCD_MODE_CMD);
+    apply_lcd_control(cfg);
+    general_write(cfg, cfg->lcd_entrymode, LCD_MODE_CMD);
+    return_home(cfg);
+}
+
+void
+apply_lcd_control(lcd_config *cfg)
+{
+    general_write(cfg, cfg->lcd_control, LCD_MODE_CMD);
+}
+
+void
+string_write(lcd_config *cfg, char str[])
+{
+    for(int i = 0; i < strlen(str); i++) { 
+	general_write(cfg, str[i], LCD_MODE_DATA);
+    }
+}
+
+void
+clear_display(lcd_config *cfg)
+{
+    general_write(cfg, LCD_CMD_CLEAR, LCD_MODE_CMD);
+    usleep(4500);
+}
+
+void
+return_home(lcd_config *cfg)
+{
+    general_write(cfg, LCD_CMD_HOME, LCD_MODE_CMD);
+    usleep(1600);
+}
+
+void
+move_screen(lcd_config *cfg, unsigned int chars)
+{
+    for(int i = 0; i < chars; i++) {
+	general_write(cfg, (LCD_SHIFT_SET | LCD_SHIFT_DISPLAY | LCD_SHIFT_LEFT), LCD_MODE_CMD);
+	usleep(500 * 1000);
+    }
+}
+/*
+ * Cursor is moved by setting specific address in DDRAM memory.
+ * Ref. HD44780 manual pages 10-12.
+ */
+void
+set_cursor(lcd_config *cfg, unsigned int col, unsigned int row)
+{
+    if (col > 40 ) {
+	puts("Column number can't be more than 40");
+	return;
+    }
+    
+    switch (row) {
+    case 0:
+	general_write(cfg, (LCD_DDRAM_ADDR_SET | col), LCD_MODE_CMD);
+	break;
+    case 1:
+	general_write(cfg, (LCD_DDRAM_ADDR_SET | LCD_DDRAM_2ND_ROW_ADDR + col), LCD_MODE_CMD);
+	break;
+    default:
+	printf("Row values 0 and 1 supported, received %d\n", row);
+	return;
+    }
+}
+
+lcd_config
+configure(char i2c_filename[], unsigned char i2c_addr)
+{
+    int i2c_fd = -1;
+    i2c_fd = open(i2c_filename, O_RDWR);
+    if (i2c_fd < 0) { 
+	puts("Error opening i2c device");
+	exit(1);
+    }
+    
+    if (ioctl(i2c_fd, I2C_SLAVE, i2c_addr) < 0) {
+	close(i2c_fd);
+	puts("Error setting I2C device address");
+	exit(1);
+    }
+    
+    lcd_config config = {
+	.i2c_fd = i2c_fd,
+	.backlight = LCD_BACKLIGHT_ON,
+	.lcd_function = (LCD_FUNCTION_SET |
+			 LCD_FUNCTION_4BIT |
+			 LCD_FUNCTION_2LINES |
+			 LCD_FUNCTION_5x8DOTS),
+	.lcd_control = (LCD_CONTROL_SET |
+			LCD_CONTROL_DISPLAY_ON |
+			LCD_CONTROL_CURSOR_ON |
+			LCD_CONTROL_BLINK_OFF),
+	.lcd_entrymode = (LCD_ENTRYMODE_SET |
+			  LCD_ENTRYMODE_LEFT |
+			  LCD_ENTRYMODE_DDRAM_DECREMENT),
+    };
+    
+    return config;
+}
+
+int
+main(void)
+{
+    lcd_config cfg = configure("/dev/i2c-1", 0x27);
+    init_lcd(&cfg);
+    char str1[] = "First line, which is longer than second";
+    char str2[] = "Second line";
+    set_cursor(&cfg, 0, 1);
+    string_write(&cfg, str2);
+    set_cursor(&cfg, 0, 0);
+    sleep(1);
+    string_write(&cfg, str1);
+    sleep(1);
+    move_screen(&cfg, strlen(str1) - 16);
+    return_home(&cfg);
+    sleep(1);
+    clear_display(&cfg);
+    general_write(&cfg, 0xBF, LCD_MODE_DATA);
+    general_write(&cfg, (LCD_SHIFT_SET | LCD_SHIFT_CURSOR | LCD_SHIFT_RIGHT), LCD_MODE_CMD);
+    general_write(&cfg, 0xFF, LCD_MODE_DATA);
+    general_write(&cfg, (LCD_SHIFT_SET | LCD_SHIFT_CURSOR | LCD_SHIFT_RIGHT), LCD_MODE_CMD);
+    general_write(&cfg, 0xFF, LCD_MODE_DATA);
+    general_write(&cfg, (LCD_SHIFT_SET | LCD_SHIFT_CURSOR | LCD_SHIFT_RIGHT), LCD_MODE_CMD);
+    general_write(&cfg, 0xFF, LCD_MODE_DATA);
+    sleep(3);
+    clear_display(&cfg);
+    
+    for (int i = 0; i < 16; i+=2) {
+	set_cursor(&cfg, i, 0);
+	general_write(&cfg, 0xFF, LCD_MODE_DATA);
+    }
+    for (int i = 1; i < 16; i+=2) {
+	set_cursor(&cfg, i, 1);
+	general_write(&cfg, 0xFF, LCD_MODE_DATA);
+    }
+    for (int i = 0; i < 10; i++) {
+	clk_write(cfg.i2c_fd, LCD_BACKLIGHT_OFF);
+	sleep(1);
+	clk_write(cfg.i2c_fd, LCD_BACKLIGHT_ON);
+	sleep(1);
+    }
+    clear_display(&cfg);
+    cfg.backlight = LCD_BACKLIGHT_OFF;
+    cfg.lcd_control = (LCD_CONTROL_SET |
+		       LCD_CONTROL_DISPLAY_OFF |
+		       LCD_CONTROL_CURSOR_OFF |
+		       LCD_CONTROL_BLINK_OFF);
+    apply_lcd_control(&cfg);
+    close(cfg.i2c_fd);
+}
---
+++ i2c-lcd.h
@@ -0,0 +1,85 @@
+#ifndef I2C_LCD_H
+#define I2C_LCD_H
+
+/*
+ * Pin layout as it for PCF8574T i2c expander card.
+ */
+#define PIN_DB7 0x80
+#define PIN_DB6 0x40
+#define PIN_DB5 0x20
+#define PIN_DB4 0x10
+#define PIN_BL 0x08
+#define PIN_E 0x04
+#define PIN_RW 0x02
+#define PIN_RS 0x01
+
+/*
+ * Data send modes.
+ * Its either instruction/command or data.
+ */
+#define LCD_MODE_CMD 0x00
+#define LCD_MODE_DATA PIN_RS
+
+/*
+ * Display commands
+ */
+#define LCD_CMD_CLEAR 0x01
+#define LCD_CMD_HOME 0x02
+
+/*
+ * Backlight control
+ */
+#define LCD_BACKLIGHT_ON PIN_BL
+#define LCD_BACKLIGHT_OFF 0x00
+
+/*
+ * Display control instructions.
+ * Ref. HD44780 manual pages 23-28.
+ */
+#define LCD_FUNCTION_SET 0x20
+#define LCD_FUNCTION_4BIT 0x00
+#define LCD_FUNCTION_2LINES 0x08
+#define LCD_FUNCTION_5x8DOTS 0x00
+#define LCD_FUNCTION_5x10DOTS 0x04
+
+#define LCD_CONTROL_SET 0x08
+#define LCD_CONTROL_DISPLAY_ON 0x04
+#define LCD_CONTROL_DISPLAY_OFF 0x00
+#define LCD_CONTROL_CURSOR_ON 0x02
+#define LCD_CONTROL_CURSOR_OFF 0x00
+#define LCD_CONTROL_BLINK_ON 0x01
+#define LCD_CONTROL_BLINK_OFF 0x00
+
+#define LCD_ENTRYMODE_SET 0x04
+#define LCD_ENTRYMODE_LEFT 0x02
+#define LCD_ENTRYMODE_DDRAM_DECREMENT 0x00
+
+#define LCD_SHIFT_SET 0x10
+#define LCD_SHIFT_CURSOR 0x00
+#define LCD_SHIFT_DISPLAY 0x08
+#define LCD_SHIFT_RIGHT 0x04
+#define LCD_SHIFT_LEFT 0x00
+
+#define LCD_DDRAM_ADDR_SET 0x80
+#define LCD_DDRAM_2ND_ROW_ADDR 0x40
+
+typedef struct {
+    int i2c_fd;
+    unsigned char backlight;
+    unsigned char lcd_function;
+    unsigned char lcd_control;
+    unsigned char lcd_entrymode;
+} lcd_config;
+
+void general_write(lcd_config *cfg, unsigned char byte, unsigned char mode);
+void clk_write(int fd, unsigned char byte);
+void init_lcd(lcd_config *cfg);
+void string_write(lcd_config *cfg, char str[]);
+void clear_display(lcd_config *cfg);
+void return_home(lcd_config *cfg);
+void move_screen(lcd_config *cfg, unsigned int chars);
+void set_cursor(lcd_config *cfg, unsigned int col, unsigned int row);
+void apply_lcd_control(lcd_config *cfg);
+lcd_config configure(char i2c_filename[], unsigned char i2c_addr);
+
+#endif
